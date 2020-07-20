5f631894f192892db3197d56110081cf86d3572f
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-20 00:32:12 -0300

    feat(input): added input validation

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,5 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useRef, useEffect } from 'react';
+import { useField } from '@unform/core';
 import './styles.scss';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -7,10 +8,30 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: React.FC<InputProps> = ({ name, label, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
   return (
     <div className="input-container">
       <p>{label}</p>
-      <input name={name} placeholder="name" {...rest} />
+      <input
+        className={` ${!!error && 'input-error'}`}
+        ref={inputRef}
+        defaultValue={defaultValue}
+        type="text"
+        placeholder="name"
+        {...rest}
+      />
+      <div className="error-message">
+        <p>{error}</p>
+      </div>
     </div>
   );
 };
