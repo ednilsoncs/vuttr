51f2cd608ad20230dba8ec97a9d3d3dec4d7db69
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-25 16:43:26 -0300

    feat(homee): added modal for add a new tool

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,16 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
+import * as Yup from 'yup';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import { debounce } from 'lodash';
 import Button from '../../components/Button';
-import SearchInput from '../../components/Search';
+import SearchInput from '../../components/SearchInput';
+import Input from '../../components/Input';
+import TextArea from '../../components/TextArea';
 import CloseIcon from '../../assets/images/icon-close.svg';
 import './styles.scss';
 import api from '../../services/api';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 interface tools {
   id: string;
@@ -15,8 +21,10 @@ interface tools {
 }
 
 const Home: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const [tools, setTools] = useState<tools[]>();
   const [isTag, setIsTag] = useState<boolean>(false);
+  const [isAdd, setIsAdd] = useState<boolean>(true);
 
   const handleSearch = async (input: string): Promise<void> => {
     let params = {};
@@ -35,12 +43,36 @@ const Home: React.FC = () => {
     isTag,
   ]);
 
+  const handleSubmit = useCallback(async data => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        title: Yup.string().required('The title is mandatory'),
+        link: Yup.string().required('The link is mandatory'),
+        tags: Yup.string().required('The tags is mandatory'),
+        description: Yup.string().required('The description is mandatory'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+        return;
+      }
+    }
+    return;
+    api.post('tools', {});
+  }, []);
+
   useEffect(() => {
     api.get('tools').then(response => setTools(response.data));
   }, []);
+
   return (
     <div className="column home hero is-fullheight">
-      <div>
+      <div className="body">
         <div className="column">
           <h1>VUTTER</h1>
           <h2>Very Useful Tools to Remember</h2>
@@ -63,7 +95,7 @@ const Home: React.FC = () => {
               <span>search in tags only</span>
             </div>
             <div className="add column">
-              <Button>Add</Button>
+              <Button onClick={() => setIsAdd(!isAdd)}>Add</Button>
             </div>
           </div>
         </div>
@@ -71,7 +103,7 @@ const Home: React.FC = () => {
           {tools?.map(tool => (
             <div key={tool.id} className="card">
               <div className="header column">
-                <h5>{tool.title}</h5>
+                <a href={tool.link}>{tool.title}</a>
                 <div className="close">
                   <img src={CloseIcon} alt="close icon" />
                 </div>
@@ -80,12 +112,46 @@ const Home: React.FC = () => {
                 <span>{tool.description}</span>
                 <div className="tags">
                   {tool.tags.map(tag => (
-                    <span>#{tag}</span>
+                    <span key={tag}>#{tag}</span>
                   ))}
                 </div>
               </div>
             </div>
           ))}
+        </div>
+      </div>
+      <div className={`add-card modal ${isAdd && 'is-active'}`}>
+        <div className="modal-background" />
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">Add new tool</p>
+            <button type="button" onClick={() => setIsAdd(!isAdd)}>
+              <img src={CloseIcon} alt="close icon" />
+            </button>
+          </header>
+          <section className="modal-card-body">
+            <Form ref={formRef} onSubmit={handleSubmit}>
+              <Input label="Title" name="title" placeholder="nome" />
+              <Input label="Link" name="link" placeholder="link" />
+              <TextArea
+                label="Description"
+                name="description"
+                placeholder="description"
+                rows={4}
+              />
+
+              <Input label="Tags" name="tags" placeholder="tags" />
+              <div className="button-footer">
+                <Button
+                  type="submit"
+                  onClick={() => formRef.current?.submitForm()}
+                >
+                  Add tool
+                </Button>
+              </div>
+            </Form>
+          </section>
+          <footer className=".modal-card-foot" />
         </div>
       </div>
     </div>
