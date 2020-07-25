92f9da0f57dea08b320728dc5e7c98e93b752c20
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-07-25 16:41:58 -0300

    chore(components): added validation for textarea

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -1,4 +1,5 @@
-import React, { TextareaHTMLAttributes } from 'react';
+import React, { TextareaHTMLAttributes, useRef, useEffect } from 'react';
+import { useField } from '@unform/core';
 import './styles.scss';
 
 interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
@@ -7,10 +8,29 @@ interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
 }
 
 const TextArea: React.FC<TextAreaProps> = ({ name, label, ...rest }) => {
+  const textArea = useRef<HTMLTextAreaElement>(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: textArea.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
   return (
     <div className="textarea-container">
       <p>{label}</p>
-      <textarea name={name} rows={4} {...rest} />
+      <textarea
+        className={` ${!!error && 'textarea-error'}`}
+        ref={textArea}
+        defaultValue={defaultValue}
+        name={name}
+        rows={4}
+        {...rest}
+      />
+      <div className="error-message">
+        <p>{error}</p>
+      </div>
     </div>
   );
 };
